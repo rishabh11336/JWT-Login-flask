fb35af11cb867e9894c1a15f2dca3f304d7e2c86
rishabh11336 <rishabh11336@gmail.com>
2023-06-05 04:38:47 +0530

    token_required decorator not working

--- app.py
+++ app.py
@@ -1,10 +1,57 @@
-from flask import Flask
+from flask import Flask, request, jsonify, make_response, render_template, session
+import jwt
+from datetime import datetime, timedelta
+from functools import wraps
+import os
 
 app = Flask(__name__)
+key = os.urandom(24)
+app.config['SECRET_KEY'] = key
+print(key)
+
+
+def token_required(func):
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        token = request.args.get('token')
+        if not token:
+            return jsonify({'Alert!':'Token is missing'})
+        try:
+            payload = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+        except:
+            return jsonify({'Alert!':'Invalid Token!'})
+        return func(*args, **kwargs)
+    return decorated
 
 @app.route('/')
 def home():
-    return '<center><h1>Jwt login system Looking for tutorial of same</h1></center>'
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return '<center><h1>Jwt login system Authentication Done!</h1></center>'
+#publi
+@app.route('/public')
+def public():
+    return 'For Public'
+
+#Authenticated
+@app.route('/auth')
+@token_required
+def auth():
+    return 'JWT is verified. Welcome to dashboard'
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.form['username'] and request.form['password'] == '123456':
+        session['logged_in'] = True
+        token = jwt.encode({
+        'user': request.form['username'],
+        'expiration': str(datetime.utcnow() + timedelta(seconds=60))
+        },
+            app.config['SECRET_KEY'], algorithm="HS256")
+        return jsonify({'token': jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])})
+    else:
+        return make_response('Unable to verify', 403, {'www-Authenticate': 'Basic realm:"Authentication Failed!"'})
 
 if __name__ == '__main__':
     app.run(debug=True)
